6adeb91d21d12ebcbe2e5f186292c8d7571fa63f
devofficial <debasisnath85@gmail.com>
2018-04-19 15:48:46 +0000

    added mlab url

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ var commentRouter = require("./routes/comment"),
 
 
 // Connecting to the courses database
-mongoose.connect("mongodb://localhost/restaurant_databse_final");
+// mongoose.connect("mongodb://localhost/restaurant_databse_final");
+mongoose.connect("mongodb://debasis:imdev1996@ds247759.mlab.com:47759/selectrestaurant");
+
 
 //Configuring the bodyParse and view engine
 app.use(flash());
